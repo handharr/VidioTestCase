3b221840a94a2bc03f5d6f0242c9651c72b75d5f
Puras Handharmahua <48229056+handharr@users.noreply.github.com>
2021-07-03 21:03:02 +0700

    Initial commit

---
+++ VidioTestCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VidioTestCase
+//
+//  Created by Puras Handharmahua on 03/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
