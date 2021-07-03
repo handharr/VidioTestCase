c6a0ca5f08ff97487d880d56e2fd623566cc28a9
Puras Handharmahua <48229056+handharr@users.noreply.github.com>
2021-07-04 00:19:53 +0700

    Set Collection View Layout

--- VidioTestCase/Feature/HomePage/Controller/HomeViewController.swift
+++ VidioTestCase/Feature/HomePage/Controller/HomeViewController.swift
@@ -9,10 +9,100 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var homeCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupCollectionView()
     }
+    
+    private func setupCollectionView() {
+        homeCollectionView.delegate = self
+        homeCollectionView.dataSource = self
+        homeCollectionView.collectionViewLayout = createLayout()
+        
+        homeCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+    }
+}
 
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return section == 0 ? 5 : 10
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let defaultCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        defaultCell.backgroundColor = .systemPink
+        defaultCell.layer.cornerRadius = 8
+        
+        return defaultCell
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        
+        return UICollectionViewCompositionalLayout { section, _ in
+            if section == 0 {
+                // item
+                let item = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .fractionalHeight(1)
+                    )
+                )
+                // group
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(0.9),
+                        heightDimension: .absolute(200)
+                    ),
+                    subitem: item,
+                    count: 1
+                )
+                group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+                
+                // section
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+                section.orthogonalScrollingBehavior = .groupPaging
+                return section
+            }
+            else if section == 1 {
+                // item
+                let item = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1/2),
+                        heightDimension: .fractionalHeight(1)
+                    )
+                )
+                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+                
+                // group
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .absolute(250)
+                    ),
+                    subitem: item,
+                    count: 2
+                )
+                group.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0)
+
+                // section
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+                return section
+            }
+            
+            return nil
+        }
+    }
 }
